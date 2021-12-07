315e66820468cca17ec3125d9ac0b0d7159b6f3f
ahernot <70817470+ahernot@users.noreply.github.com>
2021-12-07 12:07:37 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,12 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from data import Data
+from functions import moving_avg_kernel
 
 from preferences import *
+from visualisation import plot_df
 
 data = Data.from_folder('src/data-2', labels=label_dict, features=SELECTED_FEATURES)
-data.split_train_test()
-data.make_windows(window_size=100)
+
+
+data.preprocess()
+
+data_train, data_test = data.split_train_test()
+# data.make_windows(window_size=100)
 
 PRINT_DATA_LEN = False
 if PRINT_DATA_LEN:
@@ -48,6 +54,26 @@ if PLOT_DATA:
     plt.show()
 
 
+for key, df in data['train']['clean'].items():
+
+    plt.figure(figsize=(15, 10))
+    # plt.plot(df.index, df['Pouls'])
+    
+    # Average
+    avg = np.mean(df['Pouls'])
+    # plt.plot(df.index, np.ones_like(df.index) * avg)
+
+    # Moving average
+    avg_rad = 5
+    mak = moving_avg_kernel(radius=avg_rad)
+    m_avg = np.convolve(df['Pouls'].to_numpy(), mak, mode='valid')    
+    plt.plot(df.index[avg_rad:-avg_rad], m_avg)
+
+    plt.show()
+
+    # break
+
+
 
 PLOT_FOURIER = False
 if PLOT_FOURIER:
@@ -70,43 +96,33 @@ if PLOT_FOURIER:
 
 
 
+CLASSIFIERS = False
+if CLASSIFIERS:
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.neighbors import KNeighborsClassifier
+
+    data_sel = data['train']['clean']
+    # X_clean = np.array([ data_sel[data_key] [['Pouls', 'SpO2']].to_numpy() for data_key in list(data_sel.keys()) ])
+    X_clean = np.array([ np.concatenate((data_sel[data_key]['Pouls'].to_numpy(), data_sel[data_key]['SpO2'].to_numpy())) for data_key in list(data_sel.keys()) ])
+    print(X_clean.shape)
+    y_clean = np.zeros (X_clean.shape[0], dtype=np.int)
+
+    data_sel = data['train']['anomaly']
+    # X_anomaly = np.array([ data_sel[data_key] [['Pouls', 'SpO2']].to_numpy() for data_key in list(data_sel.keys()) ])
+    X_anomaly = np.array([ np.concatenate((data_sel[data_key]['Pouls'].to_numpy(), data_sel[data_key]['SpO2'].to_numpy())) for data_key in list(data_sel.keys()) ])
+    y_anomaly = np.ones (X_anomaly.shape[0], dtype=np.int)
+    print(X_anomaly.shape)
 
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
+    data_sel = data['train']['attack']
+    # X_attack = np.array([ data_sel[data_key] [['Pouls', 'SpO2']].to_numpy() for data_key in list(data_sel.keys()) ])
+    X_attack = np.array([ np.concatenate((data_sel[data_key]['Pouls'].to_numpy(), data_sel[data_key]['SpO2'].to_numpy())) for data_key in list(data_sel.keys()) ])
+    y_attack = np.ones (X_attack.shape[0], dtype=np.int) * 2
+    print(X_attack.shape)
 
-data_sel = data['train']['clean']
-# X_clean = np.array([ data_sel[data_key] [['Pouls', 'SpO2']].to_numpy() for data_key in list(data_sel.keys()) ])
-X_clean = np.array([ np.concatenate((data_sel[data_key]['Pouls'].to_numpy(), data_sel[data_key]['SpO2'].to_numpy())) for data_key in list(data_sel.keys()) ])
-print(X_clean.shape)
-y_clean = np.zeros (X_clean.shape[0], dtype=np.int)
-
-data_sel = data['train']['anomaly']
-# X_anomaly = np.array([ data_sel[data_key] [['Pouls', 'SpO2']].to_numpy() for data_key in list(data_sel.keys()) ])
-X_anomaly = np.array([ np.concatenate((data_sel[data_key]['Pouls'].to_numpy(), data_sel[data_key]['SpO2'].to_numpy())) for data_key in list(data_sel.keys()) ])
-y_anomaly = np.ones (X_anomaly.shape[0], dtype=np.int)
-print(X_anomaly.shape)
+    X = np.concatenate((X_clean, X_anomaly, X_attack), axis=0)
+    y = np.concatenate((y_clean, y_anomaly, y_attack), axis=0)
 
 
-data_sel = data['train']['attack']
-# X_attack = np.array([ data_sel[data_key] [['Pouls', 'SpO2']].to_numpy() for data_key in list(data_sel.keys()) ])
-X_attack = np.array([ np.concatenate((data_sel[data_key]['Pouls'].to_numpy(), data_sel[data_key]['SpO2'].to_numpy())) for data_key in list(data_sel.keys()) ])
-y_attack = np.ones (X_attack.shape[0], dtype=np.int) * 2
-print(X_attack.shape)
-
-X = np.concatenate((X_clean, X_anomaly, X_attack), axis=0)
-y = np.concatenate((y_clean, y_anomaly, y_attack), axis=0)
-
-print(X.shape)
-print(y.shape)
-
-# random_forest = RandomForestClassifier(n_estimators=100, max_depth=10)
-# random_forest.fit(X, y)
-
-# lr = LogisticRegression()
-# lr.fit(X, y)
-
-knn = KNeighborsClassifier()
-knn.fit(X, y)
 
