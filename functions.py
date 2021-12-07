8a70b43802c7db9923160b3a4a54c0908a02295a
ahernot <70817470+ahernot@users.noreply.github.com>
2021-12-07 12:07:29 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 def clamp (val, start, stop):
     """
@@ -6,3 +8,9 @@ def clamp (val, start, stop):
     if val < start:  return start
     elif val > stop: return stop
     else:            return val
+
+
+
+def moving_avg_kernel (radius: int = 0):
+    diameter = max(int(radius * 2 + 1), 1)
+    return np.ones(diameter) / diameter
